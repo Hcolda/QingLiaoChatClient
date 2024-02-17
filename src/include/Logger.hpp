42a993a557af257a7fcc4b6308797f55c88b3d99
quqiOnfree <2098332747@qq.com>
2024-02-17 12:09:31 +0800

    tab改为space

--- src/include/Logger.hpp
+++ src/include/Logger.hpp
@@ -16,166 +16,166 @@
 
 namespace Log
 {
-	template<typename T>
-	concept StreamType = requires (T t) {
-		std::cout << t;
-	};
+    template<typename T>
+    concept StreamType = requires (T t) {
+        std::cout << t;
+    };
 
-	class Logger
-	{
-	public:
-		enum class LogMode
-		{
-			LogINFO = 0,
-			LogWARNING,
-			LogERROR,
-			LogCRITICAL,
-			LogDEBUG
-		};
+    class Logger
+    {
+    public:
+        enum class LogMode
+        {
+            LogINFO = 0,
+            LogWARNING,
+            LogERROR,
+            LogCRITICAL,
+            LogDEBUG
+        };
 
-		Logger() :
-			m_isRunning(true),
-			m_thread(std::bind(&Logger::workFunction, this))
-		{
-			if (!openFile())
-				throw std::runtime_error("Can't no open log file.");
-		}
+        Logger() :
+            m_isRunning(true),
+            m_thread(std::bind(&Logger::workFunction, this))
+        {
+            if (!openFile())
+                throw std::runtime_error("Can't no open log file.");
+        }
 
-		~Logger()
-		{
-			m_isRunning = false;
-			m_cv.notify_all();
+        ~Logger()
+        {
+            m_isRunning = false;
+            m_cv.notify_all();
 
-			if (m_thread.joinable())
-				m_thread.join();
-		}
+            if (m_thread.joinable())
+                m_thread.join();
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		constexpr void info(Args&&... args)
-		{
-			print(LogMode::LogINFO, std::forward<Args>(args)...);
-		}
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        constexpr void info(Args&&... args)
+        {
+            print(LogMode::LogINFO, std::forward<Args>(args)...);
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		constexpr void warning(Args&&... args)
-		{
-			print(LogMode::LogWARNING, std::forward<Args>(args)...);
-		}
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        constexpr void warning(Args&&... args)
+        {
+            print(LogMode::LogWARNING, std::forward<Args>(args)...);
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		constexpr void error(Args&&... args)
-		{
-			print(LogMode::LogERROR, std::forward<Args>(args)...);
-		}
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        constexpr void error(Args&&... args)
+        {
+            print(LogMode::LogERROR, std::forward<Args>(args)...);
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		constexpr void critical(Args&&... args)
-		{
-			print(LogMode::LogCRITICAL, std::forward<Args>(args)...);
-		}
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        constexpr void critical(Args&&... args)
+        {
+            print(LogMode::LogCRITICAL, std::forward<Args>(args)...);
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		constexpr void debug(Args&&... args)
-		{
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        constexpr void debug(Args&&... args)
+        {
 #ifdef _DEBUG
-			print(LogMode::LogDEBUG, std::forward<Args>(args)...);
+            print(LogMode::LogDEBUG, std::forward<Args>(args)...);
 #endif // _DEBUG
-		}
+        }
 
-		template<typename... Args>
-			requires ((StreamType<Args>) && ...)
-		void print(LogMode mode, Args&&... args)
-		{
-			std::unique_lock<std::mutex> lock(m_mutex);
-			m_msgQueue.push(std::bind([mode = std::move(mode), this](auto... args) {
-				std::string modeString;
+        template<typename... Args>
+            requires ((StreamType<Args>) && ...)
+        void print(LogMode mode, Args&&... args)
+        {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_msgQueue.push(std::bind([mode = std::move(mode), this](auto... args) {
+                std::string modeString;
 
-				switch (mode)
-				{
-				case Logger::LogMode::LogINFO:
-					modeString = "[INFO]";
-					break;
-				case Logger::LogMode::LogWARNING:
-					modeString = "[WRANING]";
-					break;
-				case Logger::LogMode::LogERROR:
-					modeString = "[ERROR]";
-					break;
-				case Logger::LogMode::LogCRITICAL:
-					modeString = "[CRITICAL]";
-					break;
-				case Logger::LogMode::LogDEBUG:
-					modeString = "[DEBUG]";
-					break;
-				default:
-					break;
-				}
+                switch (mode)
+                {
+                case Logger::LogMode::LogINFO:
+                    modeString = "[INFO]";
+                    break;
+                case Logger::LogMode::LogWARNING:
+                    modeString = "[WRANING]";
+                    break;
+                case Logger::LogMode::LogERROR:
+                    modeString = "[ERROR]";
+                    break;
+                case Logger::LogMode::LogCRITICAL:
+                    modeString = "[CRITICAL]";
+                    break;
+                case Logger::LogMode::LogDEBUG:
+                    modeString = "[DEBUG]";
+                    break;
+                default:
+                    break;
+                }
 
-				std::cout << generateTimeFormatString() << modeString;
-				((std::cout << args), ...);
-				std::cout << '\n';
+                std::cout << generateTimeFormatString() << modeString;
+                ((std::cout << args), ...);
+                std::cout << '\n';
 
-				m_file << generateTimeFormatString() << modeString;
-				((m_file << std::forward<decltype(args)>(args)), ...);
-				m_file << std::endl;
-				}, std::forward<Args>(args)...));
-			m_cv.notify_all();
-		}
+                m_file << generateTimeFormatString() << modeString;
+                ((m_file << std::forward<decltype(args)>(args)), ...);
+                m_file << std::endl;
+                }, std::forward<Args>(args)...));
+            m_cv.notify_all();
+        }
 
-	protected:
-		static std::string generateFileName()
-		{
-			auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-			std::stringstream ss;
-			ss << std::put_time(std::localtime(&t), "%Y-%m-%d.log");
-			return ss.str();
-		}
+    protected:
+        static std::string generateFileName()
+        {
+            auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+            std::stringstream ss;
+            ss << std::put_time(std::localtime(&t), "%Y-%m-%d.log");
+            return ss.str();
+        }
 
-		static std::string generateTimeFormatString()
-		{
-			auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-			std::stringstream ss;
-			ss << std::put_time(std::localtime(&t), "[%H:%M:%S]");
-			return ss.str();
-		}
+        static std::string generateTimeFormatString()
+        {
+            auto t = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+            std::stringstream ss;
+            ss << std::put_time(std::localtime(&t), "[%H:%M:%S]");
+            return ss.str();
+        }
 
-		bool openFile()
-		{
-			std::filesystem::create_directory("./logs");
-			m_file.open("./logs/" + generateFileName(), std::ios_base::app);
-			return static_cast<bool>(m_file);
-		}
+        bool openFile()
+        {
+            std::filesystem::create_directory("./logs");
+            m_file.open("./logs/" + generateFileName(), std::ios_base::app);
+            return static_cast<bool>(m_file);
+        }
 
-		void workFunction()
-		{
-			while (m_isRunning)
-			{
-				std::unique_lock<std::mutex> lock(m_mutex);
-				m_cv.wait(lock, [&]() {return !m_msgQueue.empty() || !m_isRunning; });
-				if (!m_isRunning && m_msgQueue.empty())
-					return;
-				else if (m_msgQueue.empty())
-					continue;
-				std::function<void()> getFunc = std::move(m_msgQueue.front());
-				m_msgQueue.pop();
-				getFunc();
-			}
-		}
+        void workFunction()
+        {
+            while (m_isRunning)
+            {
+                std::unique_lock<std::mutex> lock(m_mutex);
+                m_cv.wait(lock, [&]() {return !m_msgQueue.empty() || !m_isRunning; });
+                if (!m_isRunning && m_msgQueue.empty())
+                    return;
+                else if (m_msgQueue.empty())
+                    continue;
+                std::function<void()> getFunc = std::move(m_msgQueue.front());
+                m_msgQueue.pop();
+                getFunc();
+            }
+        }
 
-	private:
-		std::ofstream						m_file;
-		std::condition_variable				m_cv;
-		std::mutex							m_mutex;
-		std::atomic<bool>					m_isRunning;
-		std::queue<std::function<void()>>	m_msgQueue;
-		std::thread							m_thread;
+    private:
+        std::ofstream                       m_file;
+        std::condition_variable             m_cv;
+        std::mutex                          m_mutex;
+        std::atomic<bool>                   m_isRunning;
+        std::queue<std::function<void()>>   m_msgQueue;
+        std::thread                         m_thread;
 
-	};
+    };
 }
 
 #endif // !LOGGER_HPP
--- src/login/login.cpp
+++ src/login/login.cpp
@@ -11,103 +11,103 @@
 Login::Login(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Login)
-	, _flag(false)
+    , _flag(false)
 {
     ui->setupUi(this);
 
-	//界面设置
-	this->setWindowFlags(Qt::FramelessWindowHint);
-	this->setAttribute(Qt::WA_TranslucentBackground);
+    //界面设置
+    this->setWindowFlags(Qt::FramelessWindowHint);
+    this->setAttribute(Qt::WA_TranslucentBackground);
 
-	//关闭按钮
-	connect(ui->pushButton, &QPushButton::clicked, this, &Login::close);
-	//最小化按钮
-	connect(ui->pushButton_2, &QPushButton::clicked, this, &Login::showMinimized);
+    //关闭按钮
+    connect(ui->pushButton, &QPushButton::clicked, this, &Login::close);
+    //最小化按钮
+    connect(ui->pushButton_2, &QPushButton::clicked, this, &Login::showMinimized);
 
-	//显示密码
-	{
-		QPushButton* btn = new QPushButton;
-		btn->setIcon(QIcon(":Login/img/eye2.png"));
-		btn->setStyleSheet(R"+*(
+    //显示密码
+    {
+        QPushButton* btn = new QPushButton;
+        btn->setIcon(QIcon(":Login/img/eye2.png"));
+        btn->setStyleSheet(R"+*(
 QPushButton{
-	background:rgb(247,247,247);
-	border-radius:5px;
-	border:0px;
+    background:rgb(247,247,247);
+    border-radius:5px;
+    border:0px;
 }
 
 QPushButton:hover{
-	background:rgb(200,200,200);
+    background:rgb(200,200,200);
 }
 
 QPushButton:pressed{
-	background:rgb(200,200,200);
+    background:rgb(200,200,200);
 }
-	)+*");
-		btn->setCursor(Qt::PointingHandCursor);
+    )+*");
+        btn->setCursor(Qt::PointingHandCursor);
 
-		QWidgetAction* widgetAction = new QWidgetAction(ui->lineEdit_2);
-		widgetAction->setDefaultWidget(btn);
+        QWidgetAction* widgetAction = new QWidgetAction(ui->lineEdit_2);
+        widgetAction->setDefaultWidget(btn);
 
-		ui->lineEdit_2->addAction(widgetAction, QLineEdit::TrailingPosition);
-		btn->setVisible(false);
+        ui->lineEdit_2->addAction(widgetAction, QLineEdit::TrailingPosition);
+        btn->setVisible(false);
 
-		connect(ui->lineEdit_2, &QLineEdit::textChanged, [=]() {
-			if (ui->lineEdit_2->text().size())
-			{
-				btn->setVisible(true);
-				btn->setCursor(Qt::PointingHandCursor);
-				btn->setEnabled(true);
-			}
-			else
-			{
-				btn->setVisible(false);
-			}
-			});
-		connect(btn, &QPushButton::clicked, [=]() {
-			static bool openShowPassword = false;
+        connect(ui->lineEdit_2, &QLineEdit::textChanged, [=]() {
+            if (ui->lineEdit_2->text().size())
+            {
+                btn->setVisible(true);
+                btn->setCursor(Qt::PointingHandCursor);
+                btn->setEnabled(true);
+            }
+            else
+            {
+                btn->setVisible(false);
+            }
+            });
+        connect(btn, &QPushButton::clicked, [=]() {
+            static bool openShowPassword = false;
 
-			if (!openShowPassword)
-			{
-				openShowPassword = true;
-				ui->lineEdit_2->setEchoMode(QLineEdit::Normal);
-				btn->setIcon(QIcon(":Login/img/eye1.png"));
-			}
-			else
-			{
-				openShowPassword = false;
-				ui->lineEdit_2->setEchoMode(QLineEdit::Password);
-				btn->setIcon(QIcon(":Login/img/eye2.png"));
-			}
+            if (!openShowPassword)
+            {
+                openShowPassword = true;
+                ui->lineEdit_2->setEchoMode(QLineEdit::Normal);
+                btn->setIcon(QIcon(":Login/img/eye1.png"));
+            }
+            else
+            {
+                openShowPassword = false;
+                ui->lineEdit_2->setEchoMode(QLineEdit::Password);
+                btn->setIcon(QIcon(":Login/img/eye2.png"));
+            }
 
-			});
+            });
 
-		// 登录
-		connect(ui->pushButton_3, &QPushButton::clicked, this, [=]() -> void {
-			
-			});
+        // 登录
+        connect(ui->pushButton_3, &QPushButton::clicked, this, [=]() -> void {
+            
+            });
 
-		// 注册
-		connect(ui->pushButton_4, &QPushButton::clicked, this, [=]() -> void {
-			return;
-			});
-	}
+        // 注册
+        connect(ui->pushButton_4, &QPushButton::clicked, this, [=]() -> void {
+            return;
+            });
+    }
 
-	// 设置阴影
-	{
-		QGraphicsDropShadowEffect* shadow = new QGraphicsDropShadowEffect(this);
-		shadow->setOffset(1, 1);
-		//设置阴影颜色
-		shadow->setColor(QColor("#000000"));
-		//设置阴影圆角
-		shadow->setBlurRadius(5);
-		//给嵌套QWidget设置阴影
-		ui->frame->setGraphicsEffect(shadow);
-	}
+    // 设置阴影
+    {
+        QGraphicsDropShadowEffect* shadow = new QGraphicsDropShadowEffect(this);
+        shadow->setOffset(1, 1);
+        //设置阴影颜色
+        shadow->setColor(QColor("#000000"));
+        //设置阴影圆角
+        shadow->setBlurRadius(5);
+        //给嵌套QWidget设置阴影
+        ui->frame->setGraphicsEffect(shadow);
+    }
 
-	// 毛玻璃效果
-	/*QGraphicsBlurEffect* effect = new QGraphicsBlurEffect(this);
-	effect->setBlurRadius(5);
-	ui->frame->setGraphicsEffect(effect);*/
+    // 毛玻璃效果
+    /*QGraphicsBlurEffect* effect = new QGraphicsBlurEffect(this);
+    effect->setBlurRadius(5);
+    ui->frame->setGraphicsEffect(effect);*/
 }
 
 Login::~Login()
@@ -117,28 +117,28 @@ Login::~Login()
 
 void Login::mousePressEvent(QMouseEvent* event)
 {
-	//界面移动
-	int num = (event->globalPos() - this->pos()).y();
-	if (event->button() == Qt::LeftButton && num < 50 && num > 10)
-	{
-		_flag = true;
-		_position = std::move(event->globalPos() - this->pos());
-		event->accept();
-	}
+    //界面移动
+    int num = (event->globalPos() - this->pos()).y();
+    if (event->button() == Qt::LeftButton && num < 50 && num > 10)
+    {
+        _flag = true;
+        _position = std::move(event->globalPos() - this->pos());
+        event->accept();
+    }
 }
 
 void Login::mouseMoveEvent(QMouseEvent* event)
 {
-	//界面移动
-	if (_flag)
-	{
-		this->move(event->globalPos() - _position);
-		event->accept();
-	}
+    //界面移动
+    if (_flag)
+    {
+        this->move(event->globalPos() - _position);
+        event->accept();
+    }
 }
 
 void Login::mouseReleaseEvent(QMouseEvent* event)
 {
-	//界面移动
-	_flag = false;
+    //界面移动
+    _flag = false;
 }
