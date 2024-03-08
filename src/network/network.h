d097c6122912096e957f67abb4a5c1ef55b36371
quqiOnfree <2098332747@qq.com>
2024-03-09 00:05:58 +0800

    修改Network，准备写网络部分

--- src/network/network.h
+++ src/network/network.h
@@ -38,52 +38,8 @@ namespace qls
     class Network
     {
     public:
-        struct SocketDataStructure
-        {
-            // 用于接收数据包
-            qls::Package package;
-            // 用于接收密钥
-            std::string uuid;
-
-            // 加密等级 1rsa 2aes 0无
-            std::atomic<int> has_encrypt = 0;
-            std::string AESKey;
-            std::string AESiv;
-        };
-
-        using acceptFunction = std::function<asio::awaitable<void>(tcp::socket&)>;
-        using receiveFunction = std::function<asio::awaitable<void>(tcp::socket&, std::string, std::shared_ptr<qls::DataPackage>)>;
-        using closeFunction = std::function<asio::awaitable<void>(tcp::socket&)>;
-
-        Network();
-        ~Network();
-
-        /*
-        * @brief 设置函数
-        * @param 有新连接时处理函数
-        * @param 有数据接收时处理函数
-        * @param 有连接主动关闭时的处理函数
-        */
-        [[deprecated("setFunctions 已经弃用")]] void setFunctions(acceptFunction a, receiveFunction r, closeFunction c);
-
-        /*
-        * @brief 运行network
-        * @param host 主机地址
-        * @param port 端口
-        */
-        void run(std::string_view host, unsigned short port);
-
-    private:
-        awaitable<void> echo(tcp::socket socket);
-        awaitable<void> listener();
-
-        std::string                     host_;
-        unsigned short                  port_;
-        std::unique_ptr<std::thread[]>  threads_;
-        const int                       thread_num_;
-        acceptFunction                  acceptFunction_;
-        receiveFunction                 receiveFunction_;
-        closeFunction                   closeFunction_;
+        Network() = default;
+        ~Network() = default;
     };
 }
 
